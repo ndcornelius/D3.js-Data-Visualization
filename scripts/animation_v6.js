380affa564b9e09b12c810c9bd3faecc05b3d5cb
Nathan Cornelius <cornelius1500@gmail.com>
2015-05-13 14:32:46 -0500

    Scatter plot & scrollable data

--- scripts/animation_v6.js
+++ scripts/animation_v6.js
@@ -1,4 +1,41 @@
 
+/*var dataViz = {
+    
+    viz3d : {
+        
+        height: 0,
+        width: 0,
+        margin: {},
+        renderer,
+        camera,
+        container,
+        axisRenderer,
+        axisCamera,
+        axisContainer,
+        init,
+        plot
+        
+    
+    
+    },
+
+    viz2d : {
+        
+        height: 0,
+        width: 0,
+        margin: {},
+        markers,
+        init,
+        plot;
+        
+    },
+    
+    data,
+    keys;
+    
+}*/
+
+
 var height, width, margin;
 var height3, width3;
 var data, keys, renderer, camera, container, fileData, renderer2, camera2, container2;
@@ -38,7 +75,7 @@ function init() {
     height = 170;
     width = 900;
     margin = {top: 40, left: 50, right: 20, bottom: 30}
-    width3 = window.innerWidth-600;
+    width3 = window.innerWidth-650;
     height3 =  width3*1/1.6;
     axesWidth = width3/5;
     axesHeight = width3/5;
@@ -52,13 +89,13 @@ function init() {
     container = document.getElementById('plot3d').appendChild(renderer.domElement);
     container2 = document.getElementById('axes3d').appendChild(renderer2.domElement);
     
-    camera = new THREE.PerspectiveCamera(45, width3 / height3, 1, 500);
+    camera = new THREE.PerspectiveCamera(45, width3 / height3, 1, 1000);
     camera.position.set(0, 0, 100);
     
     camera2 = new THREE.PerspectiveCamera( 50, axesWidth / axesHeight, 1, 1000 );
     camera2.up = camera.up; // important!
     
-    camera.lookAt(new THREE.Vector3(0, 0, 50));
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
     
     d3.select("#axes3d")
         .style("width", axesWidth)
@@ -174,74 +211,209 @@ function plot(id, x, y, variables) {
 
 }
 
+function ThreeDScatterPlot(a, b, t) {
+    
+    var scene = new THREE.Scene();
+    var axisScene = new THREE.Scene();
+    //scene.add(camera);
+                                
+    var xScale = d3.scale.linear()
+            .domain(d3.extent( data, function(d) {return +d[keys[b]]}))
+            .range([-15, 15]);
+            
+    console.log(d3.extent( data, function(d) {return +d[keys[b]]}))
+            
+    var yScale = d3.scale.linear()
+            .domain(d3.extent( data, function(d) {return +d[keys[t]]}).reverse())
+            .range([-15, 15]);
+            
+     console.log(d3.extent( data, function(d) {return +d[keys[b]]}))
+            
+    var tScale = d3.scale.linear()
+            .domain(d3.extent( data, function(d) {return +d[keys[a]]}).reverse())
+            .range([-15, 15]);
+            
+     console.log(d3.extent( data, function(d) {return +d[keys[b]]}))
+            
+    
+    var material = new THREE.MeshBasicMaterial({
+        color: 0x333333
+    });
+    var radius = 0.2;
+    var segments = 10;
+    var geometry = new THREE.SphereGeometry( radius, segments );
+    for (var i=0; i<data.length; i++) {
+        var circle = new THREE.Mesh( geometry, material );
+        var entry = data[i];
+        circle.position.y = tScale(entry[keys[t]]);
+        circle.position.x = xScale(entry[keys[b]]);
+        circle.position.z = yScale(entry[keys[a]]);
+        scene.add(circle)
+    }
+    
+    controls = new THREE.TrackballControls( camera, container );
+    /*var bb = new THREE.Box3()
+    bb.setFromObject(line);
+    bb.center(controls.target);*/
+    controls.target = new THREE.Vector3( 0, 0, 0 );
+    
+    axis = new THREE.AxisHelper( 100 );
+    axisScene.add( axis );
+    
+    
+    
+    function render() {
+        renderer.render(scene, camera);
+        renderer2.render(axisScene, camera2);
+    }
+    
+        
+    function animate() {
+        
+        requestAnimationFrame(animate);
+
+        controls.update();
+        
+        camera2.position.copy( camera.position );
+        camera2.position.sub( controls.target );
+        camera2.position.setLength( 300 );
+        
+        camera2.lookAt( axisScene.position );
+        
+        render();
+    }
+    
+    animate();
+    
+}
+
 function threeVarPlot(a, b, t ) {
     
     var scene = new THREE.Scene();
     var scene2 = new THREE.Scene();
     //scene.add(camera);
                                 
-            var xScale = d3.scale.linear()
-                    .domain(d3.extent( data, function(d) {return +d[keys[b]]}))
-                    .range([-15, 15]);
-                    
-            var yScale = d3.scale.linear()
-                    .domain(d3.extent( data, function(d) {return +d[keys[t]]}).reverse())
-                    .range([-15, 15]);
-                    
-            var tScale = d3.scale.linear()
-                    .domain(d3.extent( data, function(d) {return +d[keys[a]]}).reverse())
-                    .range([-15, 15]);
-                    
-            var geometry = new THREE.Geometry();
-            var material = new THREE.LineBasicMaterial({
-                color: 0x333333
-                //color: 0x4682b4
-            });
-            for (var i=0; i<data.length; i++) {
+    var xScale = d3.scale.linear()
+            .domain(d3.extent( data, function(d) {return +d[keys[b]]}))
+            .range([0, 15]);
             
-                var entry = data[i];
-                var p1 = tScale(entry[keys[t]]);
-                var p2 = xScale(entry[keys[b]]);
-                var p3 = yScale(entry[keys[a]]);
-                geometry.vertices.push(new THREE.Vector3(p1, p2, p3));
-            }
-        
-            var line = new THREE.Line(geometry, material);
-            scene.add(line);
+    var yScale = d3.scale.linear()
+            .domain(d3.extent( data, function(d) {return +d[keys[t]]}).reverse())
+            .range([0, 15]);
             
-            controls = new THREE.TrackballControls( camera, container );
-            controls.target.set( 0, 0, 0 );
+    var tScale = d3.scale.linear()
+            .domain(d3.extent( data, function(d) {return +d[keys[a]]}).reverse())
+            .range([-15, 15]);
             
-            axes2 = new THREE.AxisHelper( 100 );
-            scene2.add( axes2 );
-            
-            function render() {
-                renderer.render(scene, camera);
-                renderer2.render(scene2, camera2);
-            }
-            
-            function animate() {
-                
-                requestAnimationFrame(animate);
+    var geometry = new THREE.Geometry();
+    var material = new THREE.LineBasicMaterial({
+        color: 0x333333
+    });
+    for (var i=0; i<data.length; i++) {
+    
+        var entry = data[i];
+        var p1 = tScale(entry[keys[t]]);
+        var p2 = xScale(entry[keys[b]]);
+        var p3 = yScale(entry[keys[a]]);
+        geometry.vertices.push(new THREE.Vector3(p1, p2, p3));
+    }
 
-                controls.update();
-                
-                camera2.position.copy( camera.position );
-                camera2.position.sub( controls.target );
-                camera2.position.setLength( 300 );
-                
-                camera2.lookAt( scene2.position );
-                render();
-            }
-            
-            animate();
+    var line = new THREE.Line(geometry, material);
+    scene.add(line);
+    
+
+    
+    controls = new THREE.TrackballControls( camera, container );
+    var bb = new THREE.Box3()
+    bb.setFromObject(line);
+    bb.center(controls.target);
+    
+    axis = new THREE.AxisHelper( 100 );
+    scene2.add( axis );
+    
+        //Add axis labels ...
+    function addAxisLabels () {
+    
+        var  textGeo = new THREE.TextGeometry('X', {
+            size: 15,
+            height: 2,
+            curveSegments: 6,
+            font: "helvetiker",
+            style: "normal"
+
+        });
+        var  color = new THREE.Color();
+        color.setRGB(0, 0, 0);
+        var  textMaterial = new THREE.MeshBasicMaterial({ color: color });
+        var  text = new THREE.Mesh(textGeo , textMaterial);
+
+        text.position.x = 100;
+        text.position.y = 0;
+        text.position.z = 0;
+        text.rotation = camera2.rotation;
+        scene2.add(text);
+        
+        textGeo = new THREE.TextGeometry('Y', {
+            size: 15,
+            height: 2,
+            curveSegments: 6,
+            font: "helvetiker",
+            style: "normal"
+
+        });
+        text = new THREE.Mesh(textGeo , textMaterial);
+
+        text.position.x = 0;
+        text.position.y = 100;
+        text.position.z = 0;
+        text.rotation = camera2.rotation;
+        scene2.add(text);
+        
+        textGeo = new THREE.TextGeometry('Z', {
+            size: 15,
+            height: 2,
+            curveSegments: 6,
+            font: "helvetiker",
+            style: "normal"
+
+        });
+        text = new THREE.Mesh(textGeo , textMaterial);
+
+        text.position.x = 0;
+        text.position.y = 0;
+        text.position.z = 100;
+        text.rotation = camera2.rotation;
+        scene2.add(text);
+    }
+    
+    function render() {
+        renderer.render(scene, camera);
+        renderer2.render(scene2, camera2);
+    }
+    
+        
+    function animate() {
+        
+        requestAnimationFrame(animate);
+
+        controls.update();
+        
+        camera2.position.copy( camera.position );
+        camera2.position.sub( controls.target );
+        camera2.position.setLength( 300 );
+        
+        camera2.lookAt( scene2.position );
+        
+        render();
+    }
+    
+    animate();
 }
 
-/*function sizeChange(id) {
+//function sizeChange(id) {
     //TODO
-    d3.select('#' + id)append("g").attr("transform", "scale(" + $("#container").width()/900 + ")");
     
-}*/
+//}
 
 function zoom(id, a, b) {
     
@@ -281,7 +453,7 @@ function zoom(id, a, b) {
     }
 }
   
-    // Default data display
+    // Loads displays the default data file
 function initialData() {
     d3.text("../data/sin_cos.csv", function (csv) { 
     
@@ -290,15 +462,33 @@ function initialData() {
     });
 }
 
-function tabulate(data, keys) {
+// Function: tabulate
+// Parameters: data, maxLength
+// Takes parsed csv data and creates a custom html <div> table with the
+// labels on the left side, the first data points up to maxLength extending to the right, 
+// and a check box to the left of each label. Example:
+// 
+// -------------------------------
+// []_|_A_|_1_|_2_|_3_|_4_|_5_|...
+// []_|_B_|_1_|_2_|_3_|_4_|_5_|...
+// []_|_C_|_1_|_2_|_3_|_4_|_5_|...
+// ...
+
+
+function tabulate(data, maxLength) {
     
-    var tableData = [data[0], data[1], data[2], data[3], data[4]];
     var row;
+    var keys = d3.keys(data[0]);
+    var tableData = [];
+    
+    maxLength =  (maxLength < data.length) ? maxLength : data.length;
     
     var table = d3.select("#keys")
         .append("div")
-            .attr("class", "table")
-            .attr("id", "dataTable");
+            .attr("id","dataPreview")
+            .append("div")
+                .attr("class", "table")
+                .attr("id", "dataTable");
             
     for (var i=0; i<keys.length; i++) {
         
@@ -317,7 +507,7 @@ function tabulate(data, keys) {
                 .append("p")
                     .text(keys[i]);
         
-        for(var j=0; j<5; j++) {
+        for(var j=0; j<maxLength; j++) {
             
             var entry = data[j];
             
@@ -329,13 +519,11 @@ function tabulate(data, keys) {
     }  
 }
 
-function draw() {
+function draw(x) {
     
     var columns = [];
     
     markers = {};
-    
-    console.log("draw");
     
     for( var i=0; i<keys.length; i++) {
         box = document.getElementById(keys[i] + "box");
@@ -352,7 +540,8 @@ function draw() {
     plot("svg1", keys[0], keys[1], 1);
     plot("svg2", keys[0], keys[2], 1);
     plot("svg3", keys[1], keys[2], 2);
-    threeVarPlot(columns[0], columns[1], columns[2]);
+    
+    x === 0 ? threeVarPlot(columns[0], columns[1], columns[2]) : ThreeDScatterPlot(columns[0], columns[1], columns[2]);
     
     time = d3.extent(data, function(d) {return +d[keys[0]]});
     duration = (+time[1] - +time[0]) * 1000;
@@ -517,19 +706,19 @@ function previewData(csv) {
     fileData = d3.csv.parse(csv);
     fileKeys = d3.keys(fileData[0]);
         
-    tabulate(fileData, fileKeys);
+    tabulate(fileData, 1000);
     
 }
 
-function loadData() {
+function loadData(x) {
     
     console.log("load data");
-        		
+                
     data = fileData;
     keys = fileKeys;
     
     d3.selectAll("svg").remove();
-    draw();   
+    draw(x);   
 }
 
 // Code to open the user uploaded file using HTML5 FileReader
